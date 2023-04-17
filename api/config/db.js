51a3c74e10612ce8ad044a3a61f42681996fc76b
Marcus Ngooi <mngooi@gmail.com>
2023-04-17 16:57:59 -0400

    [ADD] Sign in, Sign out, Initial models, Initial schemas

--- api/config/db.js
+++ api/config/db.js
@@ -1,9 +1,9 @@
-// Lab 2 Exercise 1
+// Group Project
 // Author:      Marcus Ngooi (301147411)
 // Description: Configure database.
 
 module.exports = {
-    URI: 'mongodb://127.0.0.1/student-course-db',
-    sessionSecret: 'developmentSessionSecret',
-    secretKey: 'real_secret'
+    URI: 'mongodb+srv://group4_admin:FW4OY2CYL4LlKwbJ@cluster0.qyzhcms.mongodb.net/nursePatientDb?retryWrites=true&w=majority',
+    sessionSecret: 'developmentSessionSecret', // Not sure whether this is needed
+    secretKey: 'real_secret' // Not sure whether this is needed
 }
